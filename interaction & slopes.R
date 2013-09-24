a1299c24c433655070fe5c5f2db13630af91ef44
Johann Jacoby <johann.jacoby@gmail.com>
2013-09-24 11:56:44 +0200

    20130924_1156

--- interaction & slopes.R
+++ interaction & slopes.R
@@ -30,13 +30,62 @@ summary(model0); summary(model.lo); summary(model.hi)
 # print the interaction and the simple slopes of X1 @ X2=-1SD and X2=+1SD
 results0 <- summary(model0)[[4]]; results.lo <- summary(model.lo)[[4]]; results.hi <- summary(model0)[[4]]
 cat("\n",
-	"Interaction X1 * X2: b=",results0[4],", t=",results0[12],", p=",sprintf("%5.4f",results0[16]),ifelse(results0[16] < .05," *",""),"\n",
-	"Slope of X1 @ X2 = -1SD: b=",results.lo[2],", t=",results.lo[10],", p=",sprintf("%5.4f",results.lo[14]),ifelse(results.lo[14] < .05," *",""),"\n",
-	"Slope of X1 @ X2 = +1SD: b=",results.hi[2],", t=",results.hi[10],", p=",sprintf("%5.4f",results.hi[14]),ifelse(results.hi[14] < .05," *",""),"\n",
+	"Interaction X1 * X2: b=",results0[4],", t=",results0[12],", p=",
+					sprintf("%5.4f",results0[16]),ifelse(results0[16] < .05," *",""),"\n",
+	"Slope of X1 @ X2 = -1SD: b=",results.lo[2],", t=",results.lo[10],", p=",
+					sprintf("%5.4f",results.lo[14]),ifelse(results.lo[14] < .05," *",""),"\n",
+	"Slope of X1 @ X2 = +1SD: b=",results.hi[2],", t=",results.hi[10],", p=",
+					sprintf("%5.4f",results.hi[14]),ifelse(results.hi[14] < .05," *",""),"\n",
+	"absolute diff(p) = |",results.hi[14]," - ",results.lo[14],"| = ", abs(results.hi[14] - results.lo[14]),	"\n",
+	"diff(b) = ",results.hi[2]," - ",results.lo[2]," = ",results.hi[2] - results.lo[2],	"\n",	
 	sep="")
+
 
 # simple slope of X1 @ X2 = mean
 cat("Slope of X1 @ X2 = mean of X2: b=",results0[2],", t=",results0[10],", p=",sprintf("%5.4f",results0[14]),ifelse(results0[14] < .05," *",""),"\n")
 
 
+# simple slopes of X1 @ X2 = -2.5 SD and @ X1 = -0.5SD
+ds$centered.X2.lo.other <- ds$centered.X2 + 2.5*sd(ds$X2)
+ds$centered.X2.hi.other <- ds$centered.X2 + 1.5*sd(ds$X2)
+model.lo.other <- lm(Y~centered.X1*centered.X2.lo.other, ds)
+model.hi.other <- lm(Y~centered.X1*centered.X2.hi.other, ds)
 
+summary(model.lo.other); summary(model.hi.other)
+
+results.lo.other <- summary(model.lo.other)[[4]]; results.hi.other = summary(model.hi.other)[[4]]
+cat(
+	"Slope of X1 @ X2 = -2.5SD: b=",results.lo.other[2],", t=",results.lo.other[10],", p=",
+					sprintf("%5.4f",results.lo.other[14]),ifelse(results.lo.other[14] < .05," *",""),"\n",
+	"Slope of X1 @ X2 = -1.55SD: b=",results.hi.other[2],", t=",results.hi.other[10],", p=",
+					sprintf("%5.4f",results.hi.other[14]),ifelse(results.hi.other[14] < .05," *",""),"\n",
+	"abs.diff(p) = |",results.hi.other[14]," - ",results.lo.other[14],"| = ",abs(results.hi.other[14] - results.lo.other[14]),	"\n",
+	"diff(b) = ",results.hi.other[2]," - ",results.lo.other[2]," = ",results.hi.other[2] - results.lo.other[2],	"\n",	
+	sep="")
+
+
+
+exampledata.group.means <- as.data.frame(cbind(dv=c(trunc(rnorm(47,2,4)), trunc(rnorm(42,2.4,4))), group=c(rep(1,47), rep(2, 42))))
+table( exampledata.group.means$dv	)
+#comparing group means against 0
+test1 <- t.test(exampledata.group.means[which(exampledata.group.means$group==1),]$dv)
+test2 <- t.test(exampledata.group.means[which(exampledata.group.means$group==2),]$dv)
+cat(
+	"Group 1: t = ",test1$statistic,", p = ", sprintf("%20.10f",test1$p.value),"\n",
+	"Group 2: t = ",test2$statistic,", p = ", sprintf("%20.10f",test2$p.value),"\n",
+	sep="")
+
+library(gplots)
+
+barplot2(
+	tapply(exampledata.group.means$dv, exampledata.group.means$group, mean),
+	ylim = c(-1,5),
+	names.arg = c("Group 1", "Group 2"),
+	plot.ci=TRUE, ci.l=c(test1$conf.int[1], test2$conf.int[1]), ci.u=c(test1$conf.int[2], test2$conf.int[2])
+)
+
+#comparing the difference between means against 0
+test.both <- t.test(exampledata.group.means$dv, exampledata.group.means$group)
+cat(
+	"Group comparison : t = ",test.both$statistic,", p ", ifelse(sprintf("%5.4f",test.both$p.value) < .0001, "< .0001", paste(" = ", sprintf("%5.4f",test.both$p.value), sep="")),"\n",
+	sep="")
